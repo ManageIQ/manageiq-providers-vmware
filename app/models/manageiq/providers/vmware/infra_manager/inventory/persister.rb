785df1eda17139b3ff1d9b8f90eae7883bb139e9
Adam Grare <agrare@redhat.com>
2017-06-21 15:37:01 -0400

    Allow collection options to be easily overridden

--- app/models/manageiq/providers/vmware/infra_manager/inventory/persister.rb
+++ app/models/manageiq/providers/vmware/infra_manager/inventory/persister.rb
@@ -1,8 +1,19 @@
 class ManageIQ::Providers::Vmware::InfraManager::Inventory::Persister < ManagerRefresh::Inventory::Persister
   def initialize_inventory_collections
     add_inventory_collections(
-      ManageIQ::Providers::Vmware::InfraManager::Inventory::InventoryCollections,
-      %i(vms hosts resource_pools ems_clusters storages)
+      default_inventory_collections, inventory_collection_names, inventory_collection_options
     )
+  end
+
+  def default_inventory_collections
+    ManageIQ::Providers::Vmware::InfraManager::Inventory::InventoryCollections
+  end
+
+  def inventory_collection_names
+    %i(vms hosts resource_pools ems_clusters storages)
+  end
+
+  def inventory_collection_options
+    {}
   end
 end
