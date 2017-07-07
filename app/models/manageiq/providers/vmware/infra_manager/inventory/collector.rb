a3d1ad44793468475d5794a8ae8245aeafc42433
Adam Grare <agrare@redhat.com>
2017-07-07 15:18:52 -0400

    Add property collector and handle wait for updates

--- app/models/manageiq/providers/vmware/infra_manager/inventory/collector.rb
+++ app/models/manageiq/providers/vmware/infra_manager/inventory/collector.rb
@@ -1,6 +1,7 @@
 require 'rbvmomi/vim'
 
 class ManageIQ::Providers::Vmware::InfraManager::Inventory::Collector
+  include PropertyCollector
   include Vmdb::Logging
 
   attr_reader :ems, :exit_requested
@@ -54,5 +55,108 @@ class ManageIQ::Providers::Vmware::InfraManager::Inventory::Collector
   end
 
   def wait_for_updates(vim)
+    property_filter = create_property_filter(vim)
+
+    # Return if we don't receive any updates for 10 seconds break
+    # so that we can check if we are supposed to exit
+    options = RbVmomi::VIM.WaitOptions(:maxWaitSeconds => 10)
+
+    # Send the "special initial data version" i.e. an empty string
+    # so that we get all inventory back in the first update set
+    version = ""
+
+    _log.info("Refreshing initial inventory...")
+
+    initial = true
+    until exit_requested
+      update_set = vim.propertyCollector.WaitForUpdatesEx(:version => version, :options => options)
+      next if update_set.nil?
+
+      # Save the new update set version
+      version = update_set.version
+
+      property_filter_update_set = update_set.filterSet
+      next if property_filter_update_set.blank?
+
+      property_filter_update_set.each do |property_filter_update|
+        next if property_filter_update.filter != property_filter
+
+        object_update_set = property_filter_update.objectSet
+        next if object_update_set.blank?
+
+        process_object_update_set(object_update_set)
+      end
+
+      next if update_set.truncated
+
+      next unless initial
+
+      _log.info("Refreshing initial inventory...Complete")
+      initial = false
+    end
+  ensure
+    property_filter.DestroyPropertyFilter unless property_filter.nil?
+  end
+
+  def process_object_update_set(object_update_set)
+    _log.info("Processing #{object_update_set.count} updates...")
+
+    object_update_set.each do |object_update|
+      process_object_update(object_update)
+    end
+
+    _log.info("Processing #{object_update_set.count} updates...Complete")
+  end
+
+  def process_object_update(object_update)
+    managed_object = object_update.obj
+
+    case object_update.kind
+    when "enter"
+      process_object_update_enter(managed_object, object_update.changeSet)
+    when "modify"
+      process_object_update_modify(managed_object, object_update.changeSet)
+    when "leave"
+      process_object_update_leave(managed_object)
+    end
+  end
+
+  def process_object_update_enter(obj, change_set, missing_set = [])
+    process_object_update_modify(obj, change_set, missing_set)
+  end
+
+  def process_object_update_modify(_obj, change_set, _missing_set = [])
+    props = {}
+
+    change_set.each do |property_change|
+      next if property_change.nil?
+
+      case property_change.op
+      when 'add'
+        process_property_change_add(props, property_change)
+      when 'remove', 'indirectRemove'
+        process_property_change_remove(props, property_change)
+      when 'assign'
+        process_property_change_assign(props, property_change)
+      end
+    end
+  end
+
+  def process_object_update_leave(_obj)
+  end
+
+  def process_property_change_add(props, property_change)
+    name = property_change.name
+
+    props[name] = [] if props[name].nil?
+    props[name] << property_change.val
+  end
+
+  def process_property_change_remove(props, property_change)
+    props.delete(property_change.name)
+  end
+
+  def process_property_change_assign(props, property_change)
+    props[property_change.name] = property_change.val
   end
 end
---
+++ app/models/manageiq/providers/vmware/infra_manager/inventory/collector/property_collector.rb
@@ -0,0 +1,259 @@
+module ManageIQ::Providers::Vmware::InfraManager::Inventory::Collector::PropertyCollector
+  EmsRefreshPropMap = {
+    :VirtualMachine              => [
+      "availableField",
+      "config.cpuAffinity.affinitySet",
+      "config.cpuHotAddEnabled",
+      "config.cpuHotRemoveEnabled",
+      "config.defaultPowerOps.standbyAction",
+      "config.hardware.device",
+      "config.hardware.numCoresPerSocket",
+      "config.hotPlugMemoryIncrementSize",
+      "config.hotPlugMemoryLimit",
+      "config.memoryHotAddEnabled",
+      "config.version",
+      "datastore",
+      "guest.net",
+      "resourceConfig.cpuAllocation.expandableReservation",
+      "resourceConfig.cpuAllocation.limit",
+      "resourceConfig.cpuAllocation.reservation",
+      "resourceConfig.cpuAllocation.shares.level",
+      "resourceConfig.cpuAllocation.shares.shares",
+      "resourceConfig.memoryAllocation.expandableReservation",
+      "resourceConfig.memoryAllocation.limit",
+      "resourceConfig.memoryAllocation.reservation",
+      "resourceConfig.memoryAllocation.shares.level",
+      "resourceConfig.memoryAllocation.shares.shares",
+      "snapshot",
+      "summary.vm",
+      "summary.config.annotation",
+      "summary.config.ftInfo.instanceUuids",
+      "summary.config.guestFullName",
+      "summary.config.guestId",
+      "summary.config.memorySizeMB",
+      "summary.config.name",
+      "summary.config.numCpu",
+      "summary.config.template",
+      "summary.config.uuid",
+      "summary.config.vmPathName",
+      "summary.customValue",
+      "summary.guest.hostName",
+      "summary.guest.ipAddress",
+      "summary.guest.toolsStatus",
+      "summary.runtime.bootTime",
+      "summary.runtime.connectionState",
+      "summary.runtime.host",
+      "summary.runtime.powerState",
+      "summary.storage.unshared",
+      "summary.storage.committed",
+    ],
+    :ComputeResource             => [
+      "name",
+      "host",
+      "parent",
+      "resourcePool",
+    ],
+    :ClusterComputeResource      => [
+      "configuration.dasConfig.admissionControlPolicy",
+      "configuration.dasConfig.admissionControlEnabled",
+      "configuration.dasConfig.enabled",
+      "configuration.dasConfig.failoverLevel",
+      "configuration.drsConfig.defaultVmBehavior",
+      "configuration.drsConfig.enabled",
+      "configuration.drsConfig.vmotionRate",
+      "summary.effectiveCpu",
+      "summary.effectiveMemory",
+      "host",
+      "name",
+      "parent",
+      "resourcePool",
+    ],
+    :ResourcePool                => [
+      "name",
+      "parent",
+      "resourcePool",
+      "summary.config.cpuAllocation.expandableReservation",
+      "summary.config.cpuAllocation.limit",
+      "summary.config.cpuAllocation.reservation",
+      "summary.config.cpuAllocation.shares.level",
+      "summary.config.cpuAllocation.shares.shares",
+      "summary.config.memoryAllocation.expandableReservation",
+      "summary.config.memoryAllocation.limit",
+      "summary.config.memoryAllocation.reservation",
+      "summary.config.memoryAllocation.shares.level",
+      "summary.config.memoryAllocation.shares.shares",
+      "vm",
+    ],
+    :Folder                      => [
+      "childEntity",
+      "name",
+      "parent",
+    ],
+    :Datacenter                  => [
+      "datastoreFolder",
+      "hostFolder",
+      "name",
+      "networkFolder",
+      "parent",
+      "vmFolder",
+    ],
+    :HostSystem                  => [
+      "config.adminDisabled",
+      "config.consoleReservation.serviceConsoleReserved",
+      "config.hyperThread.active",
+      "config.network.consoleVnic",
+      "config.network.dnsConfig.domainName",
+      "config.network.dnsConfig.hostName",
+      "config.network.ipRouteConfig.defaultGateway",
+      "config.network.pnic",
+      "config.network.portgroup",
+      "config.network.vnic",
+      "config.network.vswitch",
+      "config.service.service",
+      "config.storageDevice.hostBusAdapter",
+      "config.storageDevice.scsiLun",
+      "config.storageDevice.scsiTopology.adapter",
+      "datastore",
+      "hardware.systemInfo.otherIdentifyingInfo",
+      "name",
+      "summary.host",
+      "summary.config.name",
+      "summary.config.product.build",
+      "summary.config.product.name",
+      "summary.config.product.osType",
+      "summary.config.product.vendor",
+      "summary.config.product.version",
+      "summary.config.vmotionEnabled",
+      "summary.hardware.cpuMhz",
+      "summary.hardware.cpuModel",
+      "summary.hardware.memorySize",
+      "summary.hardware.model",
+      "summary.hardware.numCpuCores",
+      "summary.hardware.numCpuPkgs",
+      "summary.hardware.numNics",
+      "summary.hardware.vendor",
+      "summary.quickStats.overallCpuUsage",
+      "summary.quickStats.overallMemoryUsage",
+      "summary.runtime.connectionState",
+      "summary.runtime.inMaintenanceMode",
+    ],
+    :Datastore                   => [
+      "info",
+      "host",
+      "capability.directoryHierarchySupported",
+      "capability.perFileThinProvisioningSupported",
+      "capability.rawDiskMappingsSupported",
+      "summary.accessible",
+      "summary.capacity",
+      "summary.datastore",
+      "summary.freeSpace",
+      "summary.maintenanceMode",
+      "summary.multipleHostAccess",
+      "summary.name",
+      "summary.type",
+      "summary.uncommitted",
+      "summary.url",
+      "parent",
+    ],
+    :StoragePod                  => [
+      "summary.capacity",
+      "summary.freeSpace",
+      "summary.name",
+      "childEntity",
+      "parent",
+    ],
+    :DistributedVirtualPortgroup => [
+      "summary.name",
+      "config.key",
+      "config.distributedVirtualSwitch",
+      "config.name",
+      "parent",
+      "host",
+      "tag",
+    ],
+    :DistributedVirtualSwitch    => [
+      "config.uplinkPortgroup",
+      "config.defaultPortConfig",
+      "config.numPorts",
+      "summary.name",
+      "summary.uuid",
+      "summary.host",
+      "summary.hostMember",
+      "parent",
+    ]
+  }.freeze
+
+  def create_property_filter(vim)
+    root_folder = vim.serviceContent.rootFolder
+
+    root_folder_object_spec = RbVmomi::VIM.ObjectSpec(
+      :obj => root_folder, :selectSet => select_set
+    )
+
+    spec = RbVmomi::VIM.PropertyFilterSpec(
+      :objectSet => [root_folder_object_spec],
+      :propSet   => prop_set
+    )
+
+    vim.propertyCollector.CreateFilter(:spec => spec, :partialUpdates => true)
+  end
+
+  def prop_set
+    EmsRefreshPropMap.collect do |type, props|
+      RbVmomi::VIM.PropertySpec(
+        :type    => type,
+        :all     => props.nil?,
+        :pathSet => props
+      )
+    end
+  end
+
+  def select_set
+    [
+      RbVmomi::VIM.TraversalSpec(
+        :name => 'tsFolder', :type => 'Folder', :path => 'childEntity',
+        :selectSet => [
+          RbVmomi::VIM.SelectionSpec(:name => 'tsFolder'),
+          RbVmomi::VIM.SelectionSpec(:name => 'tsDcToDsFolder'),
+          RbVmomi::VIM.SelectionSpec(:name => 'tsDcToHostFolder'),
+          RbVmomi::VIM.SelectionSpec(:name => 'tsDcToNetworkFolder'),
+          RbVmomi::VIM.SelectionSpec(:name => 'tsDcToVmFolder'),
+          RbVmomi::VIM.SelectionSpec(:name => 'tsCrToHost'),
+          RbVmomi::VIM.SelectionSpec(:name => 'tsCrToRp'),
+          RbVmomi::VIM.SelectionSpec(:name => 'tsRpToRp'),
+          RbVmomi::VIM.SelectionSpec(:name => 'tsRpToVm')
+        ]
+      ),
+      RbVmomi::VIM.TraversalSpec(
+        :name => 'tsDcToDsFolder', :type => 'Datacenter', :path => 'datastoreFolder',
+        :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')]
+      ),
+      RbVmomi::VIM.TraversalSpec(
+        :name => 'tsDcToHostFolder', :type => 'Datacenter', :path => 'hostFolder',
+        :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')]
+      ),
+      RbVmomi::VIM.TraversalSpec(
+        :name => 'tsDcToNetworkFolder', :type => 'Datacenter', :path => 'networkFolder',
+        :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')]
+      ),
+      RbVmomi::VIM.TraversalSpec(
+        :name => 'tsDcToVmFolder', :type => 'Datacenter', :path => 'vmFolder',
+        :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')]
+      ),
+      RbVmomi::VIM.TraversalSpec(
+        :name => 'tsCrToHost', :type => 'ComputeResource', :path => 'host',
+      ),
+      RbVmomi::VIM.TraversalSpec(
+        :name => 'tsCrToRp', :type => 'ComputeResource', :path => 'resourcePool',
+        :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsRpToRp')]
+      ),
+      RbVmomi::VIM.TraversalSpec(
+        :name => 'tsRpToRp', :type => 'ResourcePool', :path => 'resourcePool',
+        :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsRpToRp')]
+      ),
+      RbVmomi::VIM.TraversalSpec(
+        :name => 'tsRpToVm', :type => 'ResourcePool', :path => 'vm',
+      ),
+    ]
+  end
+end
