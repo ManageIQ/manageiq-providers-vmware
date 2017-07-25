69e417322954190bc2e0bf9022c72e42099d5276
Adam Grare <agrare@redhat.com>
2017-07-25 16:11:11 -0400

    Use methods for the different persister options
    Use methods that can be overridden by child classes to make the
    persister options easier to build.

--- app/models/manageiq/providers/vmware/infra_manager/inventory/persister/batch.rb
+++ app/models/manageiq/providers/vmware/infra_manager/inventory/persister/batch.rb
@@ -1,8 +1,9 @@
 class ManageIQ::Providers::Vmware::InfraManager::Inventory::Persister::Batch < ManageIQ::Providers::Vmware::InfraManager::Inventory::Persister
-  def inventory_collection_options
-    {
-      :complete => false,
-      :strategy => :local_db_find_missing_references,
-    }
+  def complete
+    false
+  end
+
+  def strategy
+    :local_db_find_missing_references
   end
 end
