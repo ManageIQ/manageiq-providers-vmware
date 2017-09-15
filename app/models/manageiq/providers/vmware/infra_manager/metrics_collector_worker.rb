61e8d9980938defa5a2bba62aea4620f9971e191
Adam Grare <agrare@redhat.com>
2017-09-15 13:23:27 -0400

    Add metrics collector code from re-arch

--- app/models/manageiq/providers/vmware/infra_manager/metrics_collector_worker.rb
+++ app/models/manageiq/providers/vmware/infra_manager/metrics_collector_worker.rb
@@ -1,8 +1,6 @@
 class ManageIQ::Providers::Vmware::InfraManager::MetricsCollectorWorker < ManageIQ::Providers::BaseManager::MetricsCollectorWorker
   require_nested :Runner
 
-  self.default_queue_name = "vmware"
-
   def friendly_name
     @friendly_name ||= "C&U Metrics Collector for vCenter"
   end
--- app/models/manageiq/providers/vmware/infra_manager/metrics_collector_worker/runner.rb
+++ app/models/manageiq/providers/vmware/infra_manager/metrics_collector_worker/runner.rb
@@ -1,3 +1,17 @@
 class ManageIQ::Providers::Vmware::InfraManager::MetricsCollectorWorker::Runner < ManageIQ::Providers::BaseManager::MetricsCollectorWorker::Runner
-  self.require_vim_broker = true
+  def after_initialize
+    @ems = ExtManagementSystem.find(@cfg[:ems_id])
+    do_exit("Unable to find instance for EMS ID [#{@cfg[:ems_id]}].", 1) if @ems.nil?
+    do_exit("EMS ID [#{@cfg[:ems_id]}] failed authentication check.", 1) unless @ems.authentication_check.first
+
+    @metrics_capture = @ems.class::MetricsCapture.new(@ems)
+    @metrics_capture.perf_counters_to_collect
+    @start_time = Time.now - 5.minutes
+  end
+
+  def do_work
+    metrics = @metrics_capture.perf_collect_metrics(@start_time)
+
+    @start_time = Time.now
+  end
 end
