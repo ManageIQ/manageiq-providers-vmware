c709b1996a00c9bbef8ee98067d7c605a160e03b
Bill Wei <bilwei@redhat.com>
2017-09-19 11:19:39 -0400

    Move spec fixture for OrchestrationTemplate to this repo

---
+++ spec/factories/orchestration_template.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :orchestration_template_vmware_cloud_in_xml,
+          :parent => :orchestration_template,
+          :class  => "ManageIQ::Providers::Vmware::CloudManager::OrchestrationTemplate" do
+    content File.read(ManageIQ::Providers::Vmware::Engine.root.join(*%w(spec fixtures orchestration_templates vmware_parameters_ovf.xml)))
+  end
+end
--- spec/models/manageiq/providers/vmware/cloud_manager/orchestration_template_spec.rb
+++ spec/models/manageiq/providers/vmware/cloud_manager/orchestration_template_spec.rb
@@ -1,18 +1,17 @@
 describe ManageIQ::Providers::Vmware::CloudManager::OrchestrationTemplate do
   describe ".eligible_manager_types" do
     it "lists the classes of eligible managers" do
-      ManageIQ::Providers::Vmware::CloudManager::OrchestrationTemplate.eligible_manager_types.each do |klass|
+      described_class.eligible_manager_types.each do |klass|
         expect(klass <= ManageIQ::Providers::Vmware::CloudManager).to be_truthy
       end
     end
   end
 
-  let(:valid_template) { FactoryGirl.create(:orchestration_template_vmware_cloud_with_content) }
+  let(:valid_template) { FactoryGirl.create(:orchestration_template_vmware_cloud_in_xml) }
 
   describe '#validate_format' do
     it 'passes validation if no content' do
-      template = ManageIQ::Providers::Vmware::CloudManager::OrchestrationTemplate.new
-      expect(template.validate_format).to be_nil
+      expect(subject.validate_format).to be_nil
     end
 
     it 'passes validation with correct OVF content' do
@@ -20,7 +19,7 @@ describe ManageIQ::Providers::Vmware::CloudManager::OrchestrationTemplate do
     end
 
     it 'fails validations with incorrect OVF content' do
-      template = ManageIQ::Providers::Vmware::CloudManager::OrchestrationTemplate.new(:content => "Invalid String")
+      template = described_class.new(:content => "Invalid String")
       expect(template.validate_format).not_to be_nil
     end
   end
@@ -34,7 +33,7 @@ describe ManageIQ::Providers::Vmware::CloudManager::OrchestrationTemplate do
         ems.cloud_networks << vapp_net
       end
     end
-    let(:orchestration_template) { FactoryGirl.create(:orchestration_template_vmware_cloud_with_content, :ems_id => ems.id) }
+    let(:orchestration_template) { FactoryGirl.create(:orchestration_template_vmware_cloud_in_xml, :ems_id => ems.id) }
 
     context "orchestration template OVF file" do
       it "is properly read" do
