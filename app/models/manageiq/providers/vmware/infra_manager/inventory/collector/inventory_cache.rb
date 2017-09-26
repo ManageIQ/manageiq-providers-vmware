96c0270595ad3209738489a7e773ed3fa8201b1d
James Wong <jwong@jwong-OSX.local>
2017-09-26 14:13:30 -0400

    keep full cache

--- app/models/manageiq/providers/vmware/infra_manager/inventory/collector/inventory_cache.rb
+++ app/models/manageiq/providers/vmware/infra_manager/inventory/collector/inventory_cache.rb
@@ -9,31 +9,8 @@ class ManageIQ::Providers::Vmware::InfraManager::Inventory::Collector
         end
     end
 
-    INVENTORY_CACHE_PROPERTIES = {
-      "VirtualMachine" => %w(
-        summary.config.template
-        summary.config.name
-        summary.config.uuid
-        summary.config.vmPathName
-      ),
-      "Host"           => %w(
-        config.network.dnsConfig.hostName
-        summary.config.product.name
-      ),
-      "Datastore"      => %w(
-        summary.name
-        summary.url
-      )
-    }.freeze
-
     def update_inventory_cache(obj_type, obj_ref, props)
-      properties_to_cache = INVENTORY_CACHE_PROPERTIES[obj_type]
-      return if properties_to_cache.blank?
-
-      cache = inventory_cache[obj_type][obj_ref]
-      properties_to_cache.each do |prop_key|
-        cache[prop_key] = props[prop_key]
-      end
+      inventory_cache[obj_type][obj_ref] = props
     end
   end
 end
