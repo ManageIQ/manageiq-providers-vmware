b58eef51fb5dca864dea82efcd07606b892dc48d
Adam Grare <agrare@redhat.com>
2017-11-08 14:41:07 -0500

    Merge pull request #132 from gmcculloug/normalize_network_adapter
    Properly update network hash when the first array element is nil
    (cherry picked from commit 9d93a0dd7242405220c5ea11bbaa3e043d571ae2)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1511502

---
+++ spec/models/manageiq/providers/vmware/infra_manager/provision/configuration/network_spec.rb
@@ -0,0 +1,55 @@
+describe ManageIQ::Providers::Vmware::InfraManager::Provision::Configuration::Network do
+  describe '#normalize_network_adapter_settings' do
+    let(:miq_provision) { FactoryGirl.build(:miq_provision_vmware, :options => options) }
+
+    shared_examples_for 'normalize_network_adapter_settings' do
+      it 'updates network options' do
+        miq_provision.normalize_network_adapter_settings
+
+        expect(miq_provision.options).to include(network_options)
+      end
+    end
+
+    context 'adds default adapter into networks hash' do
+      let(:options) { {:vlan => %w(network network), :mac_address => 'aa:bb:cc:dd:ee:ff'} }
+      let(:network_options)  { {:networks=>[{:network => 'network', :mac_address => 'aa:bb:cc:dd:ee:ff'}]} }
+
+      it_behaves_like 'normalize_network_adapter_settings'
+    end
+
+    context 'adds default adapter into networks hash' do
+      let(:options) { {:vlan => %w(dvs_network network)} }
+      let(:network_options)  { {:networks => [{:network => 'network', :is_dvs => true}]} }
+
+      it_behaves_like 'normalize_network_adapter_settings'
+    end
+
+    context 'adds default adapter into networks hash' do
+      let(:options) { {:vlan => %w(network network), :networks => []} }
+      let(:network_options)  { {:networks=>[{:network=>'network'}]} }
+
+      it_behaves_like 'normalize_network_adapter_settings'
+    end
+
+    context 'adds default adapter into networks hash' do
+      let(:options) { {:vlan => %w(network network), :networks => [nil]} }
+      let(:network_options)  { {:networks=>[{:network=>'network'}]} }
+
+      it_behaves_like 'normalize_network_adapter_settings'
+    end
+
+    context 'adds default adapter into networks hash' do
+      let(:options) { {:networks => [{:network => 'network'}]} }
+      let(:network_options) { {:vlan => %w(network network)} }
+
+      it_behaves_like 'normalize_network_adapter_settings'
+    end
+
+    context 'adds default adapter into networks hash' do
+      let(:options) { {:networks => [{:network => 'network', :is_dvs => true}]} }
+      let(:network_options) { {:vlan => %w(dvs_network network)} }
+
+      it_behaves_like 'normalize_network_adapter_settings'
+    end
+  end
+end
