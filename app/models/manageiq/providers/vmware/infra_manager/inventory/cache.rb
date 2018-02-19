8dcb2241c8d0c7a65e550f07c2c106cea52f70a3
Adam Grare <agrare@redhat.com>
2018-02-19 10:19:50 -0500

    Implement cache prop_change assign

--- app/models/manageiq/providers/vmware/infra_manager/inventory/cache.rb
+++ app/models/manageiq/providers/vmware/infra_manager/inventory/cache.rb
@@ -32,5 +32,74 @@ class ManageIQ::Providers::Vmware::InfraManager::Inventory::Cache
   end
 
   def process_prop_change(prop_hash, prop_change)
+    h, prop_str = hash_target(prop_hash, prop_change.name)
+    tag, key    = tag_and_key(prop_str)
+
+    case prop_change.op
+    when "add"
+    when "remove", "indirectRemove"
+    when "assign"
+      if key
+        # TODO
+        raise "Array properties aren't supported yet"
+      else
+        h[tag] = prop_change.val
+      end
+    end
+  end
+
+  def hash_target(base_hash, key_string)
+    h = base_hash
+    prop_keys = split_prop_path(key_string)
+
+    prop_keys[0...-1].each do |key|
+      key, array_key = tag_and_key(key)
+      if array_key
+        # TODO
+        raise "Array properties aren't supported yet"
+      else
+        h[key] ||= {}
+        h = h[key]
+      end
+    end
+
+    return h, prop_keys[-1]
+  end
+
+  def split_prop_path(prop_path)
+    path_array = []
+    in_key     = false
+    pc         = ""
+
+    prop_path.split(//).each do |c|
+      case c
+      when "."
+        unless in_key
+          path_array << pc
+          pc = ""
+          next
+        end
+      when "["
+        in_key = true
+      when "]"
+        in_key = false
+      end
+      pc << c
+    end
+
+    path_array << pc unless pc.empty?
+    path_array
+  end
+
+  def tag_and_key(prop_str)
+    return prop_str, nil unless prop_str.include? "["
+
+    if prop_str =~ /([^\[]+)\[([^\]]+)\]/
+      tag, key = $1, $2
+    else
+      raise "tagAndKey: malformed property string #{prop_str}"
+    end
+    key = key[1...-1] if key[0, 1] == '"' && key[-1, 1] == '"'
+    return tag, key
   end
 end
--- spec/models/manageiq/providers/vmware/infra_manager/inventory/cache_spec.rb
+++ spec/models/manageiq/providers/vmware/infra_manager/inventory/cache_spec.rb
@@ -3,14 +3,64 @@ require "rbvmomi/vim"
 describe ManageIQ::Providers::Vmware::InfraManager::Inventory::Cache do
   let(:cache) { described_class.new }
   let(:vm) { RbVmomi::VIM.VirtualMachine(nil, "vm-123") }
+  let(:device) do
+    [
+      RbVmomi::VIM::VirtualLsiLogicController(
+        :dynamicProperty    => [],
+        :key                => 1000,
+        :deviceInfo         => RbVmomi::VIM::Description(:label => "SCSI controller 0", :summary => "LSI Logic"),
+        :controllerKey      => 100,
+        :unitNumber         => 3,
+        :busNumber          => 0,
+        :device             => [2000],
+        :hotAddRemove       => true,
+        :sharedBus          => "noSharing",
+        :scsiCtlrUnitNumber => 7,
+      ),
+      RbVmomi::VIM::VirtualDisk(
+        :dynamicProperty => [],
+        :key             => 2000,
+        :deviceInfo      => RbVmomi::VIM::Description(:label => "Hard disk 1", :summary => "41,943,040 KB"),
+        :backing         => RbVmomi::VIM::VirtualDiskFlatVer2BackingInfo(
+          :fileName        => "[datastore] vm1/vm1.vmdk",
+          :datastore       => RbVmomi::VIM::Datastore(nil, "datastore-1"),
+          :diskMode        => "persistent",
+          :thinProvisioned => true,
+          :uuid            => "6000C294-264b-3f91-8e5c-8c2ebac1bfe8",
+        ),
+        :controllerKey   => 1000,
+        :unitNumber      => 0,
+        :capacityInKB    => 41_943_040,
+      ),
+    ]
+  end
+  let(:change_set) do
+    [
+      RbVmomi::VIM::PropertyChange(:dynamicProperty => [], :name => "config.hardware.device", :op => "assign", :val => device),
+      RbVmomi::VIM::PropertyChange(:dynamicProperty => [], :name => "config.version",         :op => "assign", :val => "vmx-08"),
+      RbVmomi::VIM::PropertyChange(:dynamicProperty => [], :name => "name",                   :op => "assign", :val => "vm1"),
+      RbVmomi::VIM::PropertyChange(:dynamicProperty => [], :name => "parent",                 :op => "assign", :val => RbVmomi::VIM::Folder(nil, "group-v3")),
+    ]
+  end
 
   context "#insert" do
     context "without initial change_set" do
-      it "creates an entry in the cache" do
-        cache.insert(vm)
+      before { cache.insert(vm) }
 
+      it "creates an entry in the cache" do
         expect(cache.keys).to include("VirtualMachine")
         expect(cache["VirtualMachine"].keys).to include("vm-123")
+      end
+    end
+
+    context "with an initial change_set" do
+      before { cache.insert(vm, change_set) }
+
+      it "caches the properties" do
+        props = cache["VirtualMachine"]["vm-123"]
+
+        expect(props.keys).to include("name", "parent", "config")
+        expect(props["config"]["version"]).to eq("vmx-08")
       end
     end
   end
@@ -28,11 +78,32 @@ describe ManageIQ::Providers::Vmware::InfraManager::Inventory::Cache do
 
     context "of an object not in the cache" do
       it "does nothing" do
-        cache.delete(vm)
+        expect(cache.delete(vm)).to be_nil
       end
     end
   end
 
   context "#update" do
+    context "of an object not in the cache" do
+      it "does nothing" do
+        expect(cache.update(vm, [])).to be_nil
+      end
+    end
+
+    context "of an object in the cache" do
+      before { cache.insert(vm, change_set) }
+
+      it "updates a top-level value" do
+        update_change_set = [RbVmomi::VIM::PropertyChange(:name => "name", :op => "assign", :val => "vm2")]
+        props = cache.update(vm, update_change_set)
+        expect(props["name"]).to eq("vm2")
+      end
+
+      it "updates a nested value" do
+        update_change_set = [RbVmomi::VIM::PropertyChange(:name => "config.version", :op => "assign", :val => "vmx-09")]
+        props = cache.update(vm, update_change_set)
+        expect(props["config"]["version"]).to eq("vmx-09")
+      end
+    end
   end
 end
