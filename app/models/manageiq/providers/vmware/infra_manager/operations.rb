f3ddfdb492978eea289ccd3343baa89ef533533c
Adam Grare <agrare@redhat.com>
2018-02-20 12:37:53 -0500

    Add a concurrent hash of mutex protected connections

--- app/models/manageiq/providers/vmware/infra_manager/operations.rb
+++ app/models/manageiq/providers/vmware/infra_manager/operations.rb
@@ -1,4 +1,40 @@
 require "sinatra/base"
 
 class ManageIQ::Providers::Vmware::InfraManager::Operations < Sinatra::Base
+  require_nested :Connection
+
+  def initialize
+    super
+
+    @connections      = Concurrent::Map.new
+    @connection_class = self.class::Connection
+  end
+
+  private
+
+  attr_reader :connections, :connection_class
+
+  def with_provider_connection
+    raise "Invalid connection parameters" unless validate_connection_params
+
+    connections.compute_if_absent(connection_key) { connection_class.new(*connection_params) }.with { |conn| yield conn }
+  end
+
+  def connection_param_keys
+    %i(server username password)
+  end
+
+  def connection_params
+    params.values_at(*connection_param_keys)
+  end
+
+  def connection_key
+    server, username, _ = connection_params
+    "#{server}__#{username}"
+  end
+
+  def validate_connection_params
+    server, user, password = connection_params
+    server && user && password
+  end
 end
