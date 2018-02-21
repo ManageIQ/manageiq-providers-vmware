f5dad10242638c9c7730cfe66fa39209cc62e2da
Adam Grare <agrare@redhat.com>
2018-02-21 09:28:37 -0500

    Merge pull request #183 from gildub/discovery_core_change
    Discovery namespace change
    (cherry picked from commit 5d78369a06482b046cef622f8a2432f79ecfbed9)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1552665

--- lib/manageiq/providers/vmware.rb
+++ lib/manageiq/providers/vmware.rb
@@ -1,9 +1,1 @@
 require "manageiq/providers/vmware/engine"
-
-module ManageIQ
-  module Providers
-    module Vmware
-      autoload :Discovery, 'manageiq/providers/vmware/discovery'
-    end
-  end
-end
