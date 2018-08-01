774320227f18f2c70140f6ab311c790d4b4cb037
Martin Slemr <mslemr@redhat.com>
2018-08-01 14:37:49 +0200

    Removed old InventoryCollectionDefault from persister
    Also removed methods described by core class

--- app/models/manageiq/providers/vmware/inventory/persister.rb
+++ app/models/manageiq/providers/vmware/inventory/persister.rb
@@ -1,26 +1,3 @@
 class ManageIQ::Providers::Vmware::Inventory::Persister < ManagerRefresh::Inventory::Persister
   require_nested :CloudManager
-
-  protected
-
-  def cloud
-    ManageIQ::Providers::Vmware::InventoryCollectionDefault::CloudManager
-  end
-
-  def targeted?
-    false
-  end
-
-  def strategy
-    nil
-  end
-
-  def shared_options
-    settings_options = options[:inventory_collections].try(:to_hash) || {}
-
-    settings_options.merge(
-      :strategy => strategy,
-      :targeted => targeted?,
-    )
-  end
 end
