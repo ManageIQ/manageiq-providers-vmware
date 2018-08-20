7d67082da425de3d69cef6268071c8f1e3e7cabd
Milan Zazrivec <mzazrivec@redhat.com>
2018-08-20 15:43:56 +0200

    Add plugin display name

--- lib/manageiq/providers/vmware/engine.rb
+++ lib/manageiq/providers/vmware/engine.rb
@@ -3,6 +3,10 @@ module ManageIQ
     module Vmware
       class Engine < ::Rails::Engine
         isolate_namespace ManageIQ::Providers::Vmware
+
+        def self.plugin_name
+          _('VMware Provider')
+        end
       end
     end
   end
