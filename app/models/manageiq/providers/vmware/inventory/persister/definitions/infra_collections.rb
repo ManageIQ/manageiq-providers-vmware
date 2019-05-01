501d991fa379a72c93ebefcd5d16d09e9b064007
Adam Grare <agrare@redhat.com>
2019-05-01 14:33:16 -0400

    Simplify infra collection definitions
    Make use of most of these overrides moving to core

--- app/models/manageiq/providers/vmware/inventory/persister/definitions/infra_collections.rb
+++ app/models/manageiq/providers/vmware/inventory/persister/definitions/infra_collections.rb
@@ -2,86 +2,37 @@ module ManageIQ::Providers::Vmware::Inventory::Persister::Definitions::InfraColl
   extend ActiveSupport::Concern
 
   def initialize_infra_inventory_collections
-    add_vms_and_templates
-
-    %i(customization_specs
-       ext_management_system
-       distributed_virtual_switches
-       host_hardwares
-       host_guest_devices
-       host_networks
-       host_storages
-       host_switches
-       host_system_services
-       host_operating_systems
-       host_virtual_switches
-       lans
-       miq_scsi_luns
-       miq_scsi_targets
-       storage_profiles).each do |name|
-
-      add_collection(infra, name)
-    end
-
-    add_hosts
-
-    %i(disks
-       guest_devices
-       hardwares
-       snapshots
-       operating_systems
-       ems_custom_attributes
-       networks).each do |name|
-
-      add_collection(infra,
-                     name,
-                     :parent_inventory_collections => %i(vms_and_templates))
-    end
-
-    %i(ems_clusters
-       ems_folders
-       resource_pools
-       storages).each do |name|
-
-      add_collection(infra,
-                     name,
-                     :attributes_blacklist => %i(parent))
-    end
-
+    add_collection(infra, :customization_specs)
+    add_collection(infra, :disks, :parent_inventory_collections => %i[vms_and_templates])
+    add_collection(infra, :distributed_virtual_switches)
+    add_collection(infra, :ems_clusters)
+    add_collection(infra, :ems_custom_attributes, :parent_inventory_collections => %i[vms_and_templates])
+    add_collection(infra, :ems_folders)
+    add_collection(infra, :ext_management_system)
+    add_collection(infra, :guest_devices, :parent_inventory_collections => %i[vms_and_templates])
+    add_collection(infra, :hardwares, :parent_inventory_collections => %i[vms_and_templates])
+    add_collection(infra, :hosts)
+    add_collection(infra, :host_hardwares)
+    add_collection(infra, :host_guest_devices)
+    add_collection(infra, :host_networks)
+    add_collection(infra, :host_storages)
+    add_collection(infra, :host_switches)
+    add_collection(infra, :host_system_services)
+    add_collection(infra, :host_operating_systems)
+    add_collection(infra, :host_virtual_switches)
+    add_collection(infra, :lans)
+    add_collection(infra, :miq_scsi_luns)
+    add_collection(infra, :miq_scsi_targets)
+    add_collection(infra, :networks, :parent_inventory_collections => %i[vms_and_templates])
+    add_collection(infra, :operating_systems, :parent_inventory_collections => %i[vms_and_templates])
+    add_collection(infra, :resource_pools)
+    add_collection(infra, :snapshots, :parent_inventory_collections => %i[vms_and_templates])
+    add_collection(infra, :storages)
+    add_collection(infra, :storage_profiles)
     add_collection(infra, :parent_blue_folders)
+    add_collection(infra, :vms_and_templates) { |b| b.vm_template_shared }
     add_collection(infra, :vm_parent_blue_folders)
     add_collection(infra, :vm_resource_pools)
     add_collection(infra, :root_folder_relationship)
-  end
-
-  # ------ IC provider specific definitions -------------------------
-  def add_vms_and_templates
-    add_collection(infra, :vms_and_templates) do |builder|
-      builder.add_properties(
-        :model_class            => ::VmOrTemplate,
-        :delete_method          => :disconnect_inv,
-        :attributes_blacklist   => %i(parent resource_pool),
-        :custom_reconnect_block => infra::INVENTORY_RECONNECT_BLOCK,
-      )
-      builder.add_default_values(:ems_id => manager.id)
-    end
-  end
-
-  def add_hosts
-    add_collection(infra, :hosts) do |builder|
-      builder.add_properties(
-        :attributes_blacklist => %i(parent),
-        :model_class          => ManageIQ::Providers::Vmware::InfraManager::HostEsx
-      )
-    end
-  end
-
-
-  def add_snapshots
-    add_collection(infra, :snapshots) do |builder|
-      builder.add_properties(
-        :parent_inventory_collections => %i(vms_and_templates)
-      )
-    end
   end
 end
