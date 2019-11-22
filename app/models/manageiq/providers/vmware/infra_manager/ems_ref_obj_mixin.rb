0e45e892d3d7deb891c000dde7fd3fbe043bdc6e
Adam Grare <agrare@redhat.com>
2019-11-22 11:54:39 -0500

    Handle the case where ems_ref_type is nil

--- app/models/manageiq/providers/vmware/infra_manager/ems_ref_obj_mixin.rb
+++ app/models/manageiq/providers/vmware/infra_manager/ems_ref_obj_mixin.rb
@@ -2,7 +2,7 @@ module ManageIQ::Providers::Vmware::InfraManager::EmsRefObjMixin
   extend ActiveSupport::Concern
 
   def ems_ref_obj
-    @ems_ref_obj ||= VimString.new(ems_ref, ems_ref_type, :ManagedObjectReference) if ems_ref.present?
+    @ems_ref_obj ||= VimString.new(ems_ref, ems_ref_type, :ManagedObjectReference) if ems_ref.present? && ems_ref_type.present?
   end
 
   def ems_ref=(val)
