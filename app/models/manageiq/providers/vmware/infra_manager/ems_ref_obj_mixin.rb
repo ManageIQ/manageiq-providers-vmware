4ea25779410e2544b868ff287185d45e5a99f806
Adam Grare <agrare@redhat.com>
2019-11-22 07:54:52 -0500

    Clear @ems_ref_obj if set to nil

--- app/models/manageiq/providers/vmware/infra_manager/ems_ref_obj_mixin.rb
+++ app/models/manageiq/providers/vmware/infra_manager/ems_ref_obj_mixin.rb
@@ -2,6 +2,11 @@ module ManageIQ::Providers::Vmware::InfraManager::EmsRefObjMixin
   extend ActiveSupport::Concern
 
   def ems_ref_obj
-    @ems_ref_obj ||= VimString.new(ems_ref, ems_ref_type, :ManagedObjectReference)
+    @ems_ref_obj ||= VimString.new(ems_ref, ems_ref_type, :ManagedObjectReference) if ems_ref.present?
+  end
+
+  def ems_ref=(val)
+    @ems_ref_obj = nil
+    super
   end
 end
