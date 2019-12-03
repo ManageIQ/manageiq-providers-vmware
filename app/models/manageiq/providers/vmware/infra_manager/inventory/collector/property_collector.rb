d1dd46e9b3023505fc691d8afe4955ff30e2248e
Adam Grare <agrare@redhat.com>
2019-12-03 11:25:26 -0500

    Load traversal specs from a hash

--- app/models/manageiq/providers/vmware/infra_manager/inventory/collector/property_collector.rb
+++ app/models/manageiq/providers/vmware/infra_manager/inventory/collector/property_collector.rb
@@ -49,92 +49,125 @@ module ManageIQ::Providers::Vmware::InfraManager::Inventory::Collector::Property
   end
 
   def root_folder_select_set
-    traversal_spec = [
-      folder_to_child_entity,
-      datacenter_to_datastore_folder,
-      datacenter_to_host_folder,
-      datacenter_to_network_folder,
-      datacenter_to_vm_folder,
-      compute_resource_to_host,
-      compute_resource_to_resource_pool,
-      resource_pool_to_resource_pool,
-      resource_pool_to_vm,
-    ]
+    traversal_spec_from_hash(
+      [
+        folder_to_child_entity,
+        datacenter_to_datastore_folder,
+        datacenter_to_host_folder,
+        datacenter_to_network_folder,
+        datacenter_to_vm_folder,
+        compute_resource_to_host,
+        compute_resource_to_resource_pool,
+        resource_pool_to_resource_pool,
+        resource_pool_to_vm,
+      ]
+    )
   end
 
   def license_manager_traversal_spec(license_manager)
     RbVmomi::VIM.ObjectSpec(:obj => license_manager)
   end
 
+  def traversal_spec_from_hash(hash)
+    hash.map do |traversal_spec|
+      RbVmomi::VIM.TraversalSpec(
+        :name => traversal_spec["name"],
+        :type => traversal_spec["type"],
+        :path => traversal_spec["path"],
+        :selectSet => traversal_spec["selectSet"]&.map do |name|
+          RbVmomi::VIM.SelectionSpec(:name => name)
+        end
+      )
+    end
+  end
+
   def folder_to_child_entity
-    RbVmomi::VIM.TraversalSpec(
-      :name => 'tsFolder', :type => 'Folder', :path => 'childEntity',
-      :selectSet => [
-        RbVmomi::VIM.SelectionSpec(:name => 'tsFolder'),
-        RbVmomi::VIM.SelectionSpec(:name => 'tsDcToDsFolder'),
-        RbVmomi::VIM.SelectionSpec(:name => 'tsDcToHostFolder'),
-        RbVmomi::VIM.SelectionSpec(:name => 'tsDcToNetworkFolder'),
-        RbVmomi::VIM.SelectionSpec(:name => 'tsDcToVmFolder'),
-        RbVmomi::VIM.SelectionSpec(:name => 'tsCrToHost'),
-        RbVmomi::VIM.SelectionSpec(:name => 'tsCrToRp'),
-        RbVmomi::VIM.SelectionSpec(:name => 'tsRpToRp'),
-        RbVmomi::VIM.SelectionSpec(:name => 'tsRpToVm')
+    traversal_spec = {
+      "name" => "tsFolder",
+      "type" => "Folder",
+      "path" => "childEntity",
+      "selectSet" => [
+        'tsFolder',
+        'tsDcToDsFolder',
+        'tsDcToHostFolder',
+        'tsDcToNetworkFolder',
+        'tsDcToVmFolder',
+        'tsCrToHost',
+        'tsCrToRp',
+        'tsRpToRp',
+        'tsRpToVm'
       ]
-    )
+    }
   end
 
   def datacenter_to_datastore_folder
-    RbVmomi::VIM.TraversalSpec(
-      :name => 'tsDcToDsFolder', :type => 'Datacenter', :path => 'datastoreFolder',
-      :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')]
-    )
+    traversal_spec = {
+      "name" => 'tsDcToDsFolder',
+      "type" => 'Datacenter',
+      "path" => 'datastoreFolder',
+      "selectSet" => ['tsFolder']
+    }
   end
 
   def datacenter_to_host_folder
-    RbVmomi::VIM.TraversalSpec(
-      :name => 'tsDcToHostFolder', :type => 'Datacenter', :path => 'hostFolder',
-      :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')]
-    )
+    traversal_spec = {
+      "name"      => "tsDcToHostFolder",
+      "type"      => "Datacenter",
+      "path"      => "hostFolder",
+      "selectSet" => ["tsFolder"]
+    }
   end
 
   def datacenter_to_network_folder
-    RbVmomi::VIM.TraversalSpec(
-      :name => 'tsDcToNetworkFolder', :type => 'Datacenter', :path => 'networkFolder',
-      :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')]
-    )
+    traversal_spec = {
+      "name"      => "tsDcToNetworkFolder",
+      "type"      => "Datacenter",
+      "path"      => "networkFolder",
+      "selectSet" => ["tsFolder"]
+    }
   end
 
   def datacenter_to_vm_folder
-    RbVmomi::VIM.TraversalSpec(
-      :name => 'tsDcToVmFolder', :type => 'Datacenter', :path => 'vmFolder',
-      :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')]
-    )
+    traversal_spec = {
+      "name"      => "tsDcToVmFolder",
+      "type"      => "Datacenter",
+      "path"      => "vmFolder",
+      "selectSet" => ["tsFolder"]
+    }
   end
 
   def compute_resource_to_host
-    RbVmomi::VIM.TraversalSpec(
-      :name => 'tsCrToHost', :type => 'ComputeResource', :path => 'host',
-    )
+    traversal_spec = {
+      "name" => "tsCrToHost",
+      "type" => "ComputeResource",
+      "path" => "host"
+    }
   end
 
   def compute_resource_to_resource_pool
-    RbVmomi::VIM.TraversalSpec(
-      :name => 'tsCrToRp', :type => 'ComputeResource', :path => 'resourcePool',
-      :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsRpToRp')]
-    )
+    traversal_spec = {
+      "name"      => "tsCrToRp",
+      "type"      => "ComputeResource",
+      "path"      => "resourcePool",
+      "selectSet" => ["tsRpToRp"]
+    }
   end
 
   def resource_pool_to_resource_pool
-    RbVmomi::VIM.TraversalSpec(
-      :name => 'tsRpToRp', :type => 'ResourcePool', :path => 'resourcePool',
-      :selectSet => [RbVmomi::VIM.SelectionSpec(:name => 'tsRpToRp')]
-    )
+    traversal_spec = {
+      "name"      => "tsRpToRp",
+      "type"      => "ResourcePool",
+      "path"      => "resourcePool",
+      "selectSet" => ["tsRpToRp"]
+    }
   end
 
   def resource_pool_to_vm
-    RbVmomi::VIM.TraversalSpec(
-      :name => 'tsRpToVm', :type => 'ResourcePool', :path => 'vm',
-    )
+    traversal_spec = {
+      "name"      => "tsRpToVm",
+      "type"      => "ResourcePool",
+      "path"      => "vm"
+    }
   end
 
   def process_change_set(change_set, props = {})
