7c5d6b97e33e82db833634a138ef5fa83a37df55
Adam Grare <agrare@redhat.com>
2021-02-11 12:36:10 -0500

    Vmware::InfraManager::Refresher#refresh is for dev only

--- app/models/manageiq/providers/vmware/infra_manager/refresher.rb
+++ app/models/manageiq/providers/vmware/infra_manager/refresher.rb
@@ -4,7 +4,17 @@ require 'http-access2' # Required in case it is not already loaded
 module ManageIQ::Providers
   module Vmware
     class InfraManager::Refresher < ManageIQ::Providers::BaseManager::Refresher
+      # This is a helper method to allow for developers to run a full refresh from
+      # a rails console with the typical `EmsRefresh.refresh(ems)` pattern.
+      #
+      # In production this is not used as the RefreshWorker processes full refreshes
+      # directly by restarting the collector thread, not by actually calling #refresh.
+      #
+      # If you need to force a full refresh in production mode you can still queue a full
+      # refresh with `EmsRefresh.queue_refresh(ems)` or `ems.queue_refresh`
       def refresh
+        raise NotImplementedError, "not implemented in production mode" if Rails.env.production?
+
         ems_by_ems_id.each do |_ems_id, ems|
           saver     = ems.class::Inventory::Saver.new(:threaded => false)
           collector = ems.class::Inventory::Collector.new(ems, saver)
