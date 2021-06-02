03ee8cbda2d1f2835e256b53ff90f4b3761ce224
Adam Grare <adam@grare.com>
2021-06-02 09:21:54 -0400

    Simplify child-manager persisters

--- app/models/manageiq/providers/vmware/inventory/persister/cloud_manager.rb
+++ app/models/manageiq/providers/vmware/inventory/persister/cloud_manager.rb
@@ -1,7 +1,15 @@
 class ManageIQ::Providers::Vmware::Inventory::Persister::CloudManager < ManageIQ::Providers::Vmware::Inventory::Persister
-  include ::ManageIQ::Providers::Vmware::Inventory::Persister::Definitions::CloudCollections
-
   def initialize_inventory_collections
-    initialize_cloud_inventory_collections
+    add_cloud_collection(:availability_zones)
+    add_cloud_collection(:disks)
+    add_cloud_collection(:hardwares)
+    add_cloud_collection(:miq_templates)
+    add_cloud_collection(:operating_systems)
+    add_cloud_collection(:orchestration_stacks)
+    add_cloud_collection(:snapshots)
+    add_cloud_collection(:orchestration_templates) do |builder|
+      builder.add_default_values(:ext_management_system => cloud_manager)
+    end
+    add_cloud_collection(:vms)
   end
 end
--- app/models/manageiq/providers/vmware/inventory/persister/network_manager.rb
+++ app/models/manageiq/providers/vmware/inventory/persister/network_manager.rb
@@ -1,23 +1,33 @@
 class ManageIQ::Providers::Vmware::Inventory::Persister::NetworkManager < ManageIQ::Providers::Vmware::Inventory::Persister
-  include ::ManageIQ::Providers::Vmware::Inventory::Persister::Definitions::CloudCollections
-  include ::ManageIQ::Providers::Vmware::Inventory::Persister::Definitions::NetworkCollections
-
   def initialize_inventory_collections
     initialize_cloud_inventory_collections
     initialize_network_inventory_collections
   end
 
   def initialize_cloud_inventory_collections
-    %i(availability_zones
-       orchestration_stacks
-       vms).each do |name|
+    %i[availability_zones orchestration_stacks vms].each do |name|
+      add_cloud_collection(name) { |builder| builder.add_properties(:strategy => :local_db_cache_all) }
+    end
+  end
 
-      add_collection(cloud, name) do |builder|
-        builder.add_properties(
-          :parent   => manager.parent_manager,
-          :strategy => :local_db_cache_all
-        )
-      end
+  def initialize_network_inventory_collections
+    %i[cloud_networks
+       cloud_subnets
+       cloud_subnet_network_ports
+       floating_ips
+       network_routers
+       network_ports
+       security_groups
+       load_balancers
+       load_balancer_pools
+       load_balancer_pool_members
+       load_balancer_pool_member_pools
+       load_balancer_listeners
+       load_balancer_listener_pools
+       load_balancer_health_checks
+       load_balancer_health_check_members].each do |name|
+
+      add_network_collection(name)
     end
   end
 end
