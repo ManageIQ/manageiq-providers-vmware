f343be5a96c4e5916a656dc86fec1ba93be9e713
Oleg Barenboim <chessbyte@gmail.com>
2021-06-04 15:16:05 -0400

    Convert stnadby_guest operation to use SupportsFeatureMixin

--- app/models/manageiq/providers/vmware/infra_manager/vm/operations/guest.rb
+++ app/models/manageiq/providers/vmware/infra_manager/vm/operations/guest.rb
@@ -28,10 +28,11 @@ module ManageIQ::Providers::Vmware::InfraManager::Vm::Operations::Guest
       unsupported_reason_add(:reset, unsupported_reason(:control)) unless supports?(:control)
       unsupported_reason_add(:reset, _("The VM is not powered on")) unless current_state == "on"
     end
-  end
 
-  def validate_standby_guest
-    validate_vm_control_powered_on
+    supports :standby_guest do
+      unsupported_reason_add(:standby_guest, unsupported_reason(:control)) unless supports?(:control)
+      unsupported_reason_add(:standby_guest, _("The VM is not powered on")) unless current_state == "on"
+    end
   end
 
   def raw_shutdown_guest
