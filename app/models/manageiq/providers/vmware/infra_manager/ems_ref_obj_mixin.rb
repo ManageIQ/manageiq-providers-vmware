0a011fbb2cc2dcc6409d7c90c7caaf72ee344920
Jason Frey <fryguy9@gmail.com>
2022-09-26 11:39:13 -0400

    Merge pull request #826 from agrare/fix_snapshot_ems_ref_obj_no_method_error
    Fix snapshot ems ref obj no method error
    
    (cherry picked from commit 073859af5d81eef5d3050dcfb2ea1ec06c80fda0)

--- app/models/manageiq/providers/vmware/infra_manager/ems_ref_obj_mixin.rb
+++ app/models/manageiq/providers/vmware/infra_manager/ems_ref_obj_mixin.rb
@@ -1,6 +1,8 @@
 module ManageIQ::Providers::Vmware::InfraManager::EmsRefObjMixin
   extend ActiveSupport::Concern
 
+  autoload :VimString, 'VMwareWebService/VimTypes'
+
   def ems_ref_obj
     @ems_ref_obj ||= VimString.new(ems_ref, ems_ref_type, :ManagedObjectReference) if ems_ref.present? && ems_ref_type.present?
   end
