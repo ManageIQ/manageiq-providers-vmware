7d6b7716e3941930e78122fd0f4883f100d70534
Adam Grare <adam@grare.com>
2022-09-26 10:37:22 -0400

    Refactor cloning_spec to allow for other contexts

--- spec/models/manageiq/providers/vmware/infra_manager/provision/cloning_spec.rb
+++ spec/models/manageiq/providers/vmware/infra_manager/provision/cloning_spec.rb
@@ -1,51 +1,38 @@
 describe ManageIQ::Providers::Vmware::InfraManager::Provision::Cloning do
+  let(:ems)               { FactoryBot.create(:ems_vmware_with_authentication) }
+  let(:user_admin)        { FactoryBot.create(:user_admin) }
+  let(:template)          { FactoryBot.create(:template_vmware, :name => "template1", :ext_management_system => ems, :cpu_limit => -1, :cpu_reserve => 0) }
+  let(:provision)         { FactoryBot.create(:miq_provision_vmware, :userid => user_admin.userid, :miq_request => provision_request, :source => template, :request_type => 'template', :state => 'pending', :status => 'Ok', :options => options) }
+  let(:provision_request) { FactoryBot.create(:miq_provision_request, :requester => user_admin, :src_vm_id => template.id) }
+  let(:options) do
+    {
+      :pass          => 1,
+      :vm_name       => "clone test",
+      :number_of_vms => 1,
+      :cpu_limit     => -1,
+      :cpu_reserve   => 0,
+      :src_vm_id     => [template.id, template.name],
+    }
+  end
+
   context "#dest_folder" do
-    before do
-      @os = FactoryBot.create(:operating_system)
-      @admin = FactoryBot.create(:user_admin)
-      @target_vm_name = 'clone test'
-      @options = {
-        :pass          => 1,
-        :vm_name       => @target_vm_name,
-        :number_of_vms => 1,
-        :cpu_limit     => -1,
-        :cpu_reserve   => 0
-      }
-      @ems         = FactoryBot.create(:ems_vmware_with_authentication, :api_version => '6.0')
-      @vm_template = FactoryBot.create(:template_vmware, :name => "template1", :ext_management_system => @ems, :operating_system => @os, :cpu_limit => -1, :cpu_reserve => 0)
-      @pr          = FactoryBot.create(:miq_provision_request, :requester => @admin, :src_vm_id => @vm_template.id)
-      @options[:src_vm_id] = [@vm_template.id, @vm_template.name]
-      @vm_prov = FactoryBot.create(:miq_provision_vmware, :userid => @admin.userid, :miq_request => @pr, :source => @vm_template, :request_type => 'template', :state => 'pending', :status => 'Ok', :options => @options)
-    end
-
-    let(:folder_name) { 'folder_one' }
-    let(:ems_folder)  { double('ems_folder') }
-    let(:dest_host) do
-      FactoryBot.create(:host_vmware, :ext_management_system => ems)
-    end
-
-    let(:dc_nested) do
-      EvmSpecHelper::EmsMetadataHelper.vmware_nested_folders(@ems)
-    end
-
-    let(:dest_host_nested) do
-      FactoryBot.create(:host_vmware, :ext_management_system => @ems).tap { |h| h.parent = dc_nested }
-    end
-
-    let(:vm_folder_nested) do
-      FactoryBot.create(:ems_folder, :name => 'vm', :ems_id => @ems.id).tap { |v| v.parent = dc_nested }
-    end
+    let(:folder_name)      { 'folder_one' }
+    let(:ems_folder)       { FactoryBot.create(:vmware_folder_vm) }
+    let(:dest_host)        { FactoryBot.create(:host_vmware, :ext_management_system => ems) }
+    let(:dc_nested)        { EvmSpecHelper::EmsMetadataHelper.vmware_nested_folders(ems) }
+    let(:dest_host_nested) { FactoryBot.create(:host_vmware, :ext_management_system => ems).tap { |h| h.parent = dc_nested } }
+    let(:vm_folder_nested) { FactoryBot.create(:ems_folder, :name => 'vm', :ems_id => ems.id).tap { |v| v.parent = dc_nested } }
 
     it "returns a folder if one is found" do
-      expect(EmsFolder).to receive(:find_by).and_return(:ems_folder)
-      expect(@vm_prov).to receive(:find_folder).never
-      @vm_prov.dest_folder
+      options[:placement_folder_name] = [ems_folder.id, ems_folder.name]
+      expect(provision).to receive(:find_folder).never
+      provision.dest_folder
     end
 
     it "attempts to find a usable folder if the ems_folder does not exist" do
-      @vm_prov.options[:dest_host] = [dest_host_nested.id, dest_host_nested.name]
-      expect(@vm_prov).to receive(:find_folder).once
-      @vm_prov.dest_folder
+      provision.options[:dest_host] = [dest_host_nested.id, dest_host_nested.name]
+      expect(provision).to receive(:find_folder).once
+      provision.dest_folder
     end
   end
 end
