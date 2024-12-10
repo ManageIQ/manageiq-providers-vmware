b89bf7abca63c544b1a07b43f5869889d89f6e7f
Joe Rafaniello <jrafanie@gmail.com>
2024-12-10 09:30:21 -0500

    Use extracted Spec::Shared::CassetteSecretsHelper from core

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,33 +14,7 @@ RSpec.configure do |config|
   end
 end
 
-DEFAULT_VCR_SECRETS_PATH = Pathname.new(__dir__).join("../config/secrets.defaults.yml")
-VCR_SECRETS_PATH         = Pathname.new(__dir__).join("../config/secrets.yml")
-
-def load_vcr_secrets(pathname)
-  if pathname.exist?
-    YAML.load_file(pathname)
-  else
-    {}
-  end
-end
-
-def default_vcr_secrets
-  @default_vcr_secrets ||= load_vcr_secrets(DEFAULT_VCR_SECRETS_PATH)
-end
-
-def vcr_secrets
-  @vcr_secrets ||= load_vcr_secrets(VCR_SECRETS_PATH)
-end
-
-def default_vcr_secret_by_key_path(*args)
-  default_vcr_secrets.dig(*args)
-end
-
-def vcr_secret_by_key_path(*args)
-  vcr_secrets.dig(*args) || default_vcr_secret_by_key_path(*args)
-end
-
+Object.include Spec::Shared::CassetteSecretsHelper
 VCR.configure do |config|
   # config.default_cassette_options = { :record => :all }
 
