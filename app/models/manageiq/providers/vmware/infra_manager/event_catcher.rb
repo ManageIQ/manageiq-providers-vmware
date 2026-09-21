9a8dfd76a41c11a783f045d0e01cd5b17660d4d9
ohhAndy <andy.hujze@gmail.com>
2026-09-21 12:58:45 -0400

    Drop rails_worker settings override

--- app/models/manageiq/providers/vmware/infra_manager/event_catcher.rb
+++ app/models/manageiq/providers/vmware/infra_manager/event_catcher.rb
@@ -1,5 +1,4 @@
 class ManageIQ::Providers::Vmware::InfraManager::EventCatcher < ManageIQ::Providers::BaseManager::EventCatcher
-  self.rails_worker = -> { !!worker_settings[:rails_worker] }
   self.worker_settings_paths = [
     %i[http_proxy vmwarews],
     %i[log level_vim],
